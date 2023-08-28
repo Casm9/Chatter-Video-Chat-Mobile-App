297d934cfe553e2b1408d993a4fc48cba88314f3
Mehmet Can Girgin <mcangirgin34@gmail.com>
2023-08-28 17:38:56 +0300

    backend setup

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,20 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api'], function(){
+   
+    Route::any('/login','LoginController@login');
+    Route::any('/get_profile','LoginController@get_profile')->middleware('CheckUser');
+    Route::any('/update_profile','LoginController@update_profile')->middleware('CheckUser');
+    Route::any('/bind_fcmtoken','LoginController@bind_fcmtoken')->middleware('CheckUser');
+    Route::any('/contact','LoginController@contact')->middleware('CheckUser');
+    Route::any('/upload_photo','LoginController@upload_photo')->middleware('CheckUser');
+    Route::any('/send_notice','LoginController@send_notice')->middleware('CheckUser');
+    Route::any('/get_rtc_token','AccessTokenController@get_rtc_token')->middleware('CheckUser');
+ 
 });
